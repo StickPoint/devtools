62d94300cb0b91b9c422e847227845a226f3551b
0303 <suncx@flamelephant.com>
2022-11-04 18:48:19 +0800

    TOOLS-008 更新UI （1）新增系统UI

--- src/main/java/com/stickpoint/devtools/view/control/SystemSetCenterController.java
+++ src/main/java/com/stickpoint/devtools/view/control/SystemSetCenterController.java
@@ -1,6 +1,9 @@
 package com.stickpoint.devtools.view.control;
 
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 
 /**
  * description: SystemSetCenterController
@@ -13,4 +16,13 @@ import javafx.scene.layout.AnchorPane;
 public class SystemSetCenterController {
 
     public AnchorPane rootPane;
+    public HBox exitSystem;
+
+    /**
+     * 点击了退出系统
+     */
+    @FXML
+    public void exitSystem() {
+        System.exit(0);
+    }
 }
